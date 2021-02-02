be8bf132771069fa01901081175cea208633f46b
asmedina24 <alejandramedina1215@gmail.com>
2021-02-02 14:00:05 -0300

    router

---
+++ view/templateLogIn.js
@@ -0,0 +1,9 @@
+export const logIn = () => {
+    const divHome = document.createElement('div');
+    const viewHome = `  
+    <p> Hacer loggin y registro </p>
+      `;
+    divHome.innerHTML = viewHome;
+    return divHome;
+  };
+  
