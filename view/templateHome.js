a0ff72ff01967d91a331226bff747e4965b874a2
erikaArango <enfermeraerikaarango@gmail.com>
2021-02-03 08:44:17 -0300

    template home

--- view/templateHome.js
+++ view/templateHome.js
@@ -1,3 +1,4 @@
+import home from "../functions/Home.js";
 
 export const home = () => {
   const divHome = document.createElement('div');
@@ -11,8 +12,26 @@ export const home = () => {
       <br>
       <a href="#/wall">Muro</a>
       <br>
-    </ul> 
-    `;
+  </ul> 
+  `;
+ };
   divHome.innerHTML = viewHome;
   return divHome;
-};
+
+  
+  const homeDiv = document.createElement('div');
+    const home1 = ` 
+    <div class="header-home"> 
+    <img src="../Imagenes/Tu-oportunidad-logo.png" id="logoLogin" alt="Logo">
+    <img src="../Imagenes/spain.png" id="flag" alt="flag">
+    </div>
+    <div class = "body-home">
+      <img src="../Imagenes/friends.png" id="imgFriends" alt="image">
+      <p>Bienvenida a la app del programa "Tu Oportunidad"</p>
+    </div>
+    <div class="footer-home">
+      <img src="../Imagenes/footer1.png" id="imgLogin" alt="eye">
+    </div>
+      `;
+      homeDiv.innerHTML = home1;
+ 
