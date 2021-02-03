98f4518faf2d46b82f4166e60ea4792709c13dd9
erikaArango <enfermeraerikaarango@gmail.com>
2021-02-03 09:05:36 -0300

    l

--- view/templateHome.js
+++ view/templateHome.js
@@ -17,21 +17,22 @@ export const home = () => {
  };
   divHome.innerHTML = viewHome;
   return divHome;
-
   
-  const homeDiv = document.createElement('div');
-    const home1 = ` 
-    <div class="header-home"> 
-    <img src="../Imagenes/Tu-oportunidad-logo.png" id="logoLogin" alt="Logo">
-    <img src="../Imagenes/spain.png" id="flag" alt="flag">
-    </div>
-    <div class = "body-home">
-      <img src="../Imagenes/friends.png" id="imgFriends" alt="image">
-      <p>Bienvenida a la app del programa "Tu Oportunidad"</p>
-    </div>
-    <div class="footer-home">
-      <img src="../Imagenes/footer1.png" id="imgLogin" alt="eye">
-    </div>
-      `;
-      homeDiv.innerHTML = home1;
- 
+  // export const Home = () => {
+  // const homeDiv = document.createElement('div');
+  //   const home1 = ` 
+  //   <div class="header-home"> 
+  //   <img src="../Imagenes/Tu-oportunidad-logo.png" id="logoLogin" alt="Logo">
+  //   <img src="../Imagenes/spain.png" id="flag" alt="flag">
+  //   </div>
+  //   <div class = "body-home">
+  //     <img src="../Imagenes/friends.png" id="imgFriends" alt="image">
+  //     <p>Bienvenida a la app del programa "Tu Oportunidad"</p>
+  //   </div>
+  //   <div class="footer-home">
+  //     <img src="../Imagenes/footer1.png" id="imgLogin" alt="eye">
+  //   </div>
+  //     `;
+  //     homeDiv.innerHTML = home1;
+  //     return homeDiv;
+  // };
