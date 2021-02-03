dbc32f48f08b3d26edcd3daff74de95fee1a1e8e
asmedina24 <alejandramedina1215@gmail.com>
2021-02-03 08:35:02 -0300

    menu

--- view/templateWall.js
+++ view/templateWall.js
@@ -1,9 +1,27 @@
 export const wall = () => {
-    const divHome = document.createElement('div');
-    const viewHome = `  
-  <p> hacer muro canales y chat </p>
-      `;
-    divHome.innerHTML = viewHome;
-    return divHome;
-  };
+  const divHome = document.createElement('div');
+  const viewHome = `  
+    <div id="navBar">
+     <p>Tu Oportunidad</p>
+    </div>
+      <div class="btn-option">
+        <div class="btn-comunidad">
+           <p>Comunidad</p>
+        </div>
+        <div class="btn-chat">
+          <p>Chat</p>
+        </div>
+      </div>
+    
+      <div class="buscador-wall">
+          <input type="search" class="" placeholder="Buscar en mis comunidades"/>
+      </div>
+      
+     
+ 
   
+  
+      `;
+  divHome.innerHTML = viewHome;
+  return divHome;
+};
