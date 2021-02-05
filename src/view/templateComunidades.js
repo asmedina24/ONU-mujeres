2e746bf7d16b579d5b4c9e89cfcab8820cc403a1
Cristián Vallejos <cristian.vallejos@t2b.cl>
2021-02-04 23:47:48 -0300

    bajar cambios

--- src/view/templateComunidades.js
+++ src/view/templateComunidades.js
@@ -1,7 +1,13 @@
 export const Comunidades = () => {
   const divComunity = document.createElement("div");
   const viewComunity = `  
-
+  <div class="input-group">
+        <div class="form-outline">
+         <input type="search" placeholder=" Buscar" id='search'/>
+         <button id="boton"> BUscar</button>
+          <label><i class="fas fa-search"></i></label>
+      </div>
+     </div>
       `;
   divComunity.innerHTML = viewComunity;
   return divComunity;
