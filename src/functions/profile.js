588235ecf3b5869d4b2940ef0a4b8ceaf5af81be
luzciel <luzcielmontesinos@gmail.com>
2021-02-07 18:43:20 -0300

    editar y crear perfil logica y css

--- src/functions/profile.js
+++ src/functions/profile.js
@@ -1,11 +1,12 @@
 // Funcion que guarda el perfil del usuario en firebase
-export const addCollectionProfile = (emailData, nameUser, photoUser, fullNameUser, cityUser, instagramUser, facebookUser, aboutMeUser) => {
+export const addCollectionProfile = (emailData, nameUser, photoUser, fullNameUser, cityUser, instagramUser, facebookUser, aboutMeUser, occupationUser) => {
     firebase.firestore().collection('perfil').add({
       email: emailData,
       name: nameUser,
       photo: photoUser  || '' ,
       fullName: fullNameUser,
       city: cityUser,
+      occupacition: occupationUser,
       instagram: instagramUser,
       facebook: facebookUser,
       aboutMe: aboutMeUser,
@@ -15,3 +16,25 @@ export const addCollectionProfile = (emailData, nameUser, photoUser, fullNameUse
     });
   };
   
+
+
+// Editar el post en firebase
+export const editProfileFirebase = (id, emailData, nameUser, photoUser, fullNameUser, cityUser, instagramUser, facebookUser, aboutMeUser, occupationUser) => {
+  const PostRef = firebase.firestore().collection('perfil').doc(id);
+  return PostRef.update({
+    email: emailData,
+    name: nameUser,
+    photo: photoUser  || '' ,
+    fullName: fullNameUser,
+    city: cityUser,
+    instagram: instagramUser,
+    facebook: facebookUser,
+    aboutMe: aboutMeUser,
+    occupation: occupationUser,
+  })
+    .then(() => {
+      console.log("perfil editado");
+    })
+    .catch(() => {
+    });
+};
--- src/view/templateProfile.js
+++ src/view/templateProfile.js
@@ -1,4 +1,4 @@
-// <----------Contenido del Editar Perfil--------->
+// <----------Datos del Perfil--------->
 export const profile = () => {
   const currentUserData = firebase.auth().currentUser; // Datos del Usuario que accedió
   const emailData = currentUserData.email; // Email del usuario que accedio
@@ -9,29 +9,57 @@ export const profile = () => {
       <img src="Imagenes/flecha-atras.svg" class="back"/>   
       <p class="createProfile">Mi perfil</p>
       <div id="profileData"> 
+        <div id="loader"></div>
       </div>
-        <a href="#/editProfile"><button class="button-edit-profile">Editar perfil</button></a>
     </div> 
     `;
   divProfile.innerHTML = viewProfile;
-
+  
+  const loarde = divProfile.querySelector('#loader');
   const profileData = divProfile.querySelector('#profileData'); // Llamando al div donde se imprimirán los post
   firebase.firestore().collection('perfil').where('email', '==', emailData).get().then((querySnapshot) => {
+    // console.log(2222, querySnapshot)
     querySnapshot.forEach((doc) => {
+      loarde.remove(); 
+      const profilePhotoIcon = `<img src="Imagenes/icono-avatar-photo.png" id="filePost"></img>`
       const profilePhoto = `<img src="${doc.data().photo}" class="img-post"></img>`;
+      const instagramUser = `<p>Instagram</p>
+      <p class="information-profile-instagram">${doc.data().instagram}</p>`;
+      const facebookUser = `<p>Facebook</p>
+      <p class="information-profile-facebook">${doc.data().facebook}</p>`;
+
       profileData.innerHTML += ` 
-        <div class="profile-data-user"> 
+      <div class="profile-data-user"> 
         <div class="profile-photo">
-        ${doc.data().photo === undefined ? '' : profilePhoto}
+          ${doc.data().photo === '' ? profilePhotoIcon : profilePhoto}
         </div>
-          <p class="name-profile">${doc.data().name} ${doc.data().fullName}</p>
-          <p class="information-profile">${doc.data().city}</p>
-          <p class="information-profile">${doc.data().instagram}</p>
-          <p class="information-profile">${doc.data().facebook}</p>
-          <p class="information-profile">${doc.data().aboutMe}</p>
-        </div> 
+        <p class="name-profile">${doc.data().name} ${doc.data().fullName}</p>
+        <p class="information-profile">${doc.data().occupacition}</p>
+        <p class="information-profile">${doc.data().city}</p>
+        <div class="instagram-user">
+        ${doc.data().instagram == "" ? "" : instagramUser}
+        </div>
+        <div class="facebook-user">
+          ${doc.data().facebook == "" ? "" : facebookUser}
+        </div>
+        <p class="information-profile">${doc.data().aboutMe}</p>
+      </div> 
+      <div class="form-button">
+        <a href="#/editProfile"> 
+          <button class="btn-profile-edit">Editar</button>
+        </a> 
+        <a href="#/wall">
+         <button class="btn-profile-save">Guardar</button>
+        </a>
+      </div>
     `;
     });
+     
+    const backArrow = divProfile.querySelector('.back');
+    backArrow.addEventListener('click',() => {
+      window.history.back();
+    });
+
   });
 
   return divProfile;
