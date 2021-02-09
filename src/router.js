80f4827f0a62b94a0ae9c1df49634d115ad1745e
asmedina24 <alejandramedina1215@gmail.com>
2021-02-09 12:57:04 -0300

    chat

--- src/router.js
+++ src/router.js
@@ -1,22 +1,24 @@
 import { logIn } from './view/templateLogIn.js';
 import { home } from './view/templateHome.js';
 import { profile } from './view/templateProfile.js';
-import { editProfile } from './view/templateEditProfile.js'
+import { editProfile }  from './view/templateEditProfile.js'
 import { wall } from './view/templateWall.js';
-import { Comunidades } from './view/templateComunidades.js'
-import { Chat } from './view/templateChat.js'
+import {Comunidades} from './view/templateComunidades.js'
+import {Chat} from './view/templateChat.js'
 import { crearComunidades } from "./view/templateCrearComunidades.js";
-import { canal } from './view/templateCanal.js'
+import {canal} from './view/templateCanal.js'
 import login from './functions/login.js';
-import { createNewChat } from './view/templatecreateNewChat.js'
-import { NewChat } from './view/templateNewchat.js'
+import {crearGrupoChat} from './view/templateCrearGrupoChat.js';
+import {posteoChat} from './view/templatePosteoChat.js';
+import {initchat} from './view/templateinitchat.js';
+import {buscarCorreo} from './view/templateBuscarCorreo.js'
 
 // import { message } from './view/templateMessage.js'
 
-const showtemplate = (hash, params) => {
-  console.log("showtemplate", params);
-  const containerRoot = document.getElementById('root');
-  containerRoot.innerHTML = `
+const showtemplate = (hash,params) => {
+    console.log("showtemplate" ,params);
+    const containerRoot = document.getElementById('root');
+    containerRoot.innerHTML = `
   <!-- Definimos nuestra cabecera -->
 <header>
     <!-- Nuestro botón. En el ejemplo uso Font-awesome para mostrar un icono de barras -->
@@ -40,94 +42,118 @@ const showtemplate = (hash, params) => {
 </header>  
     `;
 
-  const cerrar = document.getElementById('cerrarSesion');
-  cerrar.addEventListener("click", () => {
-    login.cerrarSesion();
+    const cerrar = document.getElementById('cerrarSesion');
+    cerrar.addEventListener("click", () => {
+        login.cerrarSesion();
 
-  });
+    });
 
-  switch (hash) {
-    case "":
-      containerRoot.innerHTML = "";
-      containerRoot.appendChild(home());
-      break;
-    case "#/home":
-      containerRoot.innerHTML = "";
-      containerRoot.appendChild(home());
-      break;
-    case "#/profile":
-      containerRoot.innerHTML = "";
-      containerRoot.appendChild(profile());
-      break;
-    case "#/logIn":
-      containerRoot.innerHTML = "";
-      containerRoot.appendChild(logIn());
-      break;
-    case "#/wall":
-      containerRoot.appendChild(wall());
-      break;
-    case '#/editProfile':
-      containerRoot.innerHTML = '';
-      containerRoot.appendChild(editProfile());
-      break;
-    case "#/message":
-      containerRoot.appendChild(message());
-      break;
-    case "#/crearComunidades":
-      containerRoot.innerHTML = '';
-      containerRoot.appendChild(crearComunidades());
-      break;
-   
-    case "#/Canal":
-      containerRoot.innerHTML = "";
-      containerRoot.appendChild(canal(params));
-      break;
-    default:
-      containerRoot.innerHTML = "<h2>La pagina que busca no existe</h2>";
-  }
+    switch (hash) {
+      case "":
+        containerRoot.innerHTML = "";
+        containerRoot.appendChild(home());
+        break;
+      case "#/home":
+        containerRoot.innerHTML = "";
+        containerRoot.appendChild(home());
+        break;
+      case "#/profile":
+        containerRoot.innerHTML = "";
+        containerRoot.appendChild(profile());
+        break;
+      case "#/logIn":
+        containerRoot.innerHTML = "";
+        containerRoot.appendChild(logIn());
+        break;
+      case "#/wall":
+        containerRoot.appendChild(wall());
+        break;
+      case '#/editProfile':
+        containerRoot.innerHTML =''; 
+        containerRoot.appendChild(editProfile());
+        break;  
+      case "#/message":
+        containerRoot.appendChild(message());
+        break;
+      case "#/crearComunidades":
+        containerRoot.innerHTML = '';
+        containerRoot.appendChild(crearComunidades());
+        break;
+        case "#/crearGrupoChat":
+          containerRoot.innerHTML = '';
+          containerRoot.appendChild(crearGrupoChat());
+          break;
+      case "#/Canal":
+        containerRoot.innerHTML = "";
+        containerRoot.appendChild(canal(params));
+        break;
+        case "#/Chat":
+         
+        containerRoot.appendChild(Chat(params));
+        break;
+        case "#/posteoChat":
+          containerRoot.innerHTML = "";
+        containerRoot.appendChild(posteoChat());
+          break;
+          case "#/initchat":
+          containerRoot.appendChild(initchat(params));
+            break;
+            case "#/buscarCorreo":
+          containerRoot.appendChild(buscarCorreo());
+            break;
+      default:
+        containerRoot.innerHTML = "<h2>La pagina que busca no existe</h2>";
+    }
 };
 
 export const changeroute = (hash) => {
-  console.log("changeroute", hash, hash.split("?"));
-  let url = hash.split("?");
-  hash = url[0];
-  let params = url[1];
-  if (hash === '') {
+  
+    // console.log("changeroute", hash, hash.split("?"));
+let url = hash.split("?");
+hash=url[0];
+let params = url[1];
+    if (hash === '') {
+        return showtemplate(hash);
+    } if (hash === '#/home') {
+        return showtemplate(hash);
+    } if (hash === '#/profile') {
+        return showtemplate(hash);
+    } if (hash === '#/editProfile') {
+        return showtemplate(hash);
+    } if (hash === '#/wall') {
+        return showtemplate(hash);
+    } if (hash === '#/message') {
+        return showtemplate(hash);
+    } if (hash === '#/logIn') {
+        return showtemplate(hash);
+    } if (hash === '#/crearComunidades') {
+        return showtemplate(hash);
+    } if (hash === '#/crearGrupoChat') {
+      return showtemplate(hash);
+    } if (hash === '#/Chat') {
     return showtemplate(hash);
-  } if (hash === '#/home') {
-    return showtemplate(hash);
-  } if (hash === '#/profile') {
-    return showtemplate(hash);
-  } if (hash === '#/editProfile') {
-    return showtemplate(hash);
-  } if (hash === '#/wall') {
-    return showtemplate(hash);
-  } if (hash === '#/message') {
-    return showtemplate(hash);
-  } if (hash === '#/logIn') {
-    return showtemplate(hash);
-  } if (hash === '#/crearComunidades') {
-    return showtemplate(hash);
-  } 
- 
-
-  return showtemplate(hash, params);
+  }if (hash === '#/posteoChat') {
+  return showtemplate(hash);
+}if (hash === '#/buscarCorreo') {
+  return showtemplate(hash);
+}
+     
+    return showtemplate(hash, params);
 };
 
 export const showTabs = (tab, div) => {
   const containerRoot = div
   console.log("showTabs", containerRoot);
-  // containerRoot.innerHTML = "";
+  containerRoot.innerHTML = "";
   switch (tab) {
     case "":
-      containerRoot.appendChild(Comunidades());
+        containerRoot.appendChild(Comunidades());
       break;
     case "Comunidades":
-      containerRoot.innerHTML = "";
+      containerRoot.innerHTML = ""; 
       containerRoot.appendChild(Comunidades());
       break;
     case "Chat":
-      containerRoot.innerHTML = "";
       containerRoot.appendChild(Chat());
       break;
     default:
--- src/view/templateChat.js
+++ src/view/templateChat.js
@@ -1,27 +1,27 @@
- import chat from "../functions/chat.js"
- export const Chat = () => {
+import chat from "../functions/chat.js"
+export const Chat = () => {
   const divChat = document.createElement("div");
   const viewChat = `  
-
   <div class="input-group">
     <div class="form-outline">
     <input type="search" placeholder=" Buscar" id='search'/>
       <label><i class="fas fa-search"></i></label>
     </div>
   </div>
-      
-  <div id="contenidoprotegido"></div>
-           
-    <!-- Default dropup button -->
-    <div class="dropup">
-      <button type="button" class="fabComunity " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-plus"></i>
-      </button>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="#/NewChat"><i class="fas fa-eye"></i> Nuevo Chat</a>
-      <a class="dropdown-item" href="#/createNewChat"><i class="fas fa-plus"></i> Crear Grupo</a>
-    </div>
-</div>`;
+        <div id="contenidoprotegido"></div>
+        <div id="contenidoprotegidoChat"></div>
+         
+        <!-- Default dropup button -->
+           <div class="dropup">
+             <button type="button" class="fabComunity " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               <i class="fas fa-ellipsis-v"></i>
+             </button>
+             <div class="dropdown-menu">
+               <a class="dropdown-item" href="#/buscarCorreo"><i class="fas fa-eye"></i> Iniciar Chat</a>
+             <a class="dropdown-item" href="#/crearGrupoChat"><i class="fas fa-plus"></i> Crear Grupo</a>
+           </div>
+       </div>
+      `;
 
 
       divChat.innerHTML = viewChat;
@@ -32,28 +32,29 @@
       // const items = divChat.querySelectorAll(".item");
       // const btnComunidad = divChat.querySelector("#btnComunidad");
       // const btnChat = divChat.querySelector("#btnChat");
+      
       // const formulario = divChat.querySelector("#formulario");
       // const answerChat = divChat.querySelector("#answerChat");
     
-      firebase.auth().onAuthStateChanged((user) => {
-      chat.mostrarChat2();
-        // const uid = user.uid;
-        // const name = user.displayName;
-        // const img = user.photoURL;
-        // const date = new Date();
-        // const fecha = `${
-        //   (`00${date.getDate()}`).slice(-2)}/${(`00${date.getMonth() + 1}`).slice(-2)}/${
-        //   date.getFullYear()} ${
-        //   (`00${date.getHours()}`).slice(-2)}:${
-        //   (`00${date.getMinutes()}`).slice(-2)}:${
-        //   (`00${date.getSeconds()}`).slice(-2)}`;
+       firebase.auth().onAuthStateChanged((user) => {
+      //   const uid = user.uid;
+      //   const name = user.displayName;
+      //   const img = user.photoURL;
+      //   const date = new Date();
+      //   const fecha = `${
+      //     (`00${date.getDate()}`).slice(-2)}/${(`00${date.getMonth() + 1}`).slice(-2)}/${
+      //     date.getFullYear()} ${
+      //     (`00${date.getHours()}`).slice(-2)}:${
+      //     (`00${date.getMinutes()}`).slice(-2)}:${
+      //     (`00${date.getSeconds()}`).slice(-2)}`;
     
-        //         formulario.addEventListener('submit', (e) => {
-        //         e.preventDefault()
-        //           chat.guardarChart(name, answerChat, uid, fecha, img);
-        //       });
-        //       //  chat.mostrarChat2(uid);
+      //         //   formulario.addEventListener('submit', (e) => {
+      //         //   e.preventDefault()
+      //         //     chat.guardarChart(name, answerChat, uid, fecha, img);
+      //         // });
+       chat.mostrarChat();
+      //  chat.mostrarChatUnico();
               
-      });       
+       });       
    return divChat;
 };
